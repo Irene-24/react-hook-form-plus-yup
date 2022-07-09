57ef9f549a8dcdbe5e783c6a6fcd7812829bf7fc
Irene-24 <okaforirenen@gmail.com>
2022-07-09 21:07:27 +0100

    part one code base

---
+++ schemas/intro.js
@@ -0,0 +1,30 @@
+import * as yup from "yup";
+
+const userSchema = yup
+  .object()
+  .shape({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .email("Please enter a valid email")
+      .required("Email is required"),
+    age: yup
+      .number()
+      .typeError("Must be a number")
+      .required("Please enter a valid age")
+      .integer("Age must be a whole number")
+      .moreThan(0, "Age must be greater than 0"),
+    location: yup.string().required("Location is required"),
+    mode: yup.string().required("Work mode is required"),
+  })
+  .required();
+
+const userInit = {
+  name: "",
+  email: "",
+  age: "",
+  location: "",
+  mode: "",
+};
+
+export { userInit, userSchema };
